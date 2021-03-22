c38be645856a0a51c79ab62dd42b2617a61741fd
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-03-22 18:36:41 +0300

    i need somebody`s help

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+def welcome_user():
+    import prompt
+
+    name = prompt.string("May I have your name?")
+    print("Hello, " + name + "!")
+
+if __name__ == "__main__":
+    welcome_user()
