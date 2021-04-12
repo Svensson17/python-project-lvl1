301cfbdfaadf1eb3c3768dc14f51c51a2b3d92c8
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-04-12 11:59:41 +0300

    work block

--- brain_games/brain_even_game.py
+++ brain_games/brain_even_game.py
@@ -1,14 +1,12 @@
 import random
 
+import prompt
+
 
 def question():
     num = random.randint(0,100)
     print("Question: ", num)
     return num
-
-
-    import prompt
-
 
 def answer():
     answer = prompt.string("Your answer: ")
