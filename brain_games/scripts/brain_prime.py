bbed15b82c4951757b40785562a9ee0e013b1e7b
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-05-16 12:47:47 +0300

    fix bugz

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -19,7 +19,7 @@ def main():
             print("Correct!")
             score = score + 1
         else:
-            print("{0} is wrong answer ;(. Correct answer was {1}".format(
+            print("'{0}' is wrong answer ;(. Correct answer was '{1}'".format(
                 ans,
                 right_answer))
             print("Let's try again, {0}!".format(
