234d4ca42b96b53e3edde5a27407a57e4d9d349e
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-06-23 22:27:07 +0300

    fatality calc2

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -9,12 +9,12 @@ def get_question_and_answer():
     num2 = random.randint(0, 100)
     symbol = random.randint(1, 3)
     if symbol == 1:
-        question = num1 + " + " + num2
-        right_answer = num1 + num2
+        question = str(num1) + " + " + str(num2)
+        right_answer = str(num1 + num2)
     elif symbol == 2:
-        question = num1 + " - " + num2
-        right_answer = num1 - num2
+        question = str(num1) + " - " + str(num2)
+        right_answer = str(num1 - num2)
     elif symbol == 3:
-        question = num1 + " * " + num2
-        right_answer = num1 * num2
+        question = str(num1) + " * " + str(num2)
+        right_answer = str(num1 * num2)
     return question, right_answer
