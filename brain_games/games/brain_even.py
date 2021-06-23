82ed5fedae009f5585e4af1cdb5339304315e703
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-06-23 22:04:42 +0300

    fatality2

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,5 +6,5 @@ RULE = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 def get_question_and_answer():
     question = random.randint(0, 100)
-    right_answer = "yes" if num % 2 == 0 else "no"
+    right_answer = "yes" if question % 2 == 0 else "no"
     return question, right_answer
