67cb0cfc5bbe00a7518fab6b919cca9eab8c81aa
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-06-24 20:26:01 +0300

    sub-zero prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,16 +6,16 @@ RULE = "Answer \"yes\" if the number is prime. Otherwise answer \"no\"."
 
 def get_question_and_answer():
     question = random.randint(0, 100)
-    right_answer = "yes" if True else "no"
+    right_answer = "yes" if is_prime(question) else "no"
     return question, right_answer
 
 
-    def is_prime(question):
-        if question < 2:
+def is_prime(num):
+    if num < 2:
+        return False
+    divider = 2
+    while divider <= num / 2:
+        if num % divider == 0:
             return False
-        divider = 2
-        while divider <= question / 2:
-            if question % divider == 0:
-                return False
-            divider += 1
-        return True
+        divider += 1
+    return True
