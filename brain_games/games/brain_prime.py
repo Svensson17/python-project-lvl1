40a723bc44699948ab99ef8624b013e631a46f7b
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-06-24 19:52:10 +0300

    mortal prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,18 +4,17 @@ import random
 RULE = "Answer \"yes\" if the number is prime. Otherwise answer \"no\"."
 
 
-def is_prime(num):
-    if num < 2:
-        return False
-    divider = 2
-    while divider <= num / 2:
-        if num % divider == 0:
-            return False
-        divider += 1
-    return True
-
-
 def get_question_and_answer():
     question = random.randint(0, 100)
-    right_answer = "yes" if is_prime() else "no"
+
+    def is_prime(question):
+        if question < 2:
+            return False
+        divider = 2
+        while divider <= question / 2:
+            if question % divider == 0:
+                return False
+            divider += 1
+        return True
+    right_answer = "yes" if True else "no"
     return question, right_answer
