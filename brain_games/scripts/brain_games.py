7bae4a54e8bd89576f890eee15212e1a4b3d8a1a
Kiryl Shvedau <kiryl.shvedau@gmail.com>
2021-06-27 21:08:16 +0300

    final fantasy3

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.welcome_user import welcome
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome()
+
+
+if __name__ == "__main__":
+    main()
